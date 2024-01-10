ad3aa285761c9e1f7c91256ecd0dd19f3b46448f
nurrochman-ramadhon <nurrochman.acc@gmail.com>
2024-01-10 23:40:14 +0700

    feat: create models and generate table addresses, carts, cities, order_details, orders, provinces

---
+++ apps/api/src/migrations/20240110163656-create-carts.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      account_id: {
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      warehouse_id: {
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      total_price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('carts');
+  }
+};
---
+++ apps/api/src/models/carts.js
@@ -0,0 +1,32 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default class carts extends Model {
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  static associate(models) {
+    // define association here
+    carts.belongsTo(models.accounts, { foreignKey: "account_id" })
+    carts.belongsTo(models.products, { foreignKey: "product_id" })
+    carts.belongsTo(models.warehouses, { foreignKey: "warehouses_id" })
+  }
+}
+
+export const init = (sequelize) => {
+  carts.init(
+    {
+      account_id: DataTypes.INTEGER,
+      product_id: DataTypes.INTEGER,
+      warehouse_id: DataTypes.INTEGER,
+      quantity: DataTypes.INTEGER,
+      total_price: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'carts',
+    },
+  );
+  return carts;
+};
